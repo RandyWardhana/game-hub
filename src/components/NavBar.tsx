c6c2f455b7d31a49d982ac89ee76b4be738764cc
Randy Wardhana <randy.wardhana@meditap.id>
2023-10-31 10:56:36 +0700

    Add react router dom

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <HStack justifyContent='space-between' padding='10px'>
-      <Image src={logo} boxSize='60px' />
+      <Link to='/'>
+        <Image src={logo} boxSize='60px' />
+      </Link>
       <ColorModeSwitch />
     </HStack>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import theme from './theme'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <BrowserRouter>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
 )
