4fa7de6ff4ac0310ad8660bbb7f5ebdde570772a
Randy Wardhana <randy.wardhana@meditap.id>
2023-11-09 15:30:41 +0700

    Using react query to get genres

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import App from './App'
 import theme from './theme'
+
 import './index.css'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <BrowserRouter>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
